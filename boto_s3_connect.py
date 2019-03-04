302e5201cdb85b249c502f7d398f856a4e3e0769
nagaraj <nagaraj@nsrinivasablrmbp.com>
2019-03-04 10:31:50 +0530

    New file to check boto bucket status

---
+++ boto_s3_connect.py
@@ -0,0 +1,34 @@
+mport env
+import sys
+import boto
+from boto.s3.connection import S3Connection
+from util.misc.cloud_credentials import CloudCredentials
+from boto.s3.key import Key
+from boto.exception import S3DataError
+c=CloudCredentials()
+k=c.get_aws_keys()
+aws_key = None
+secret_key = None
+ep="s3.amazonaws.com"
+
+aws_key = 'AKIAIXS5UZSPRPRLV3VQ'
+secret_key = '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'
+
+print aws_key, secret_key
+#'AKIAIXS5UZSPRPRLV3VQ', 'vWERGs1ZiqBEDqOBDLMKJRR7EYDmFTmfdhqG8UDn'
+s3conn = S3Connection(aws_key, secret_key, host=ep)
+if not s3conn:
+  print "ERROR: s3conn failed"
+  sys.exit(1)
+bucket = s3conn.get_bucket("ntnx-3461279735544769517-77221342941104975-9")
+if not bucket:
+  print "ERROR: bucket not found"
+  sys.exit(1)
+key=Key(bucket)
+key.key = 'health_check_key'
+try:
+  n=key.set_contents_from_string('health_check_key')
+except S3DataError as e:
+  print "error %s" % e
+  sys.exit(1)
+print n, "succeeded"
