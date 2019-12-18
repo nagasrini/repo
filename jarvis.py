15543158b63de26c86cf0be5103f576710eaa14f
nagaraj.yedathore <nagaraj@nutanix.com>
2019-12-18 12:50:33 +0530

    Changed to work on a cluster as well.

--- jarvis.py
+++ jarvis.py
@@ -3,8 +3,6 @@ import json
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-no_glags = False
-
 base_url="https://jarvis.eng.nutanix.com/api/v1"
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -23,7 +21,7 @@ def get_user_id(name="nagaraj.srinivasa"):
   if not r.ok:
     return None
   d = json.loads(r.content)
-  if not len(d):
+  if not len(d) or not len(d['data']):
     return None
   #print(d['data'])
   uid = d['data'][0]['_id']['$oid']
@@ -65,20 +63,28 @@ def get_clusters(uid):
 
 if __name__ == "__main__":
   try:
-    import gflags
     sys.path.append('/home/nutanix/cluster/bin')
     import env
+    import gflags
+    gflags.DEFINE_string("name", "", "name")
     FLAGS = gflags.FLAGS
     argv = FLAGS(sys.argv)
     name = FLAGS.name
-  except:
+  except Exception as e:
     if len(sys.argv) > 1 and sys.argv[1].split('=')[0] == 'name':
       name = sys.argv[1].split('=')[1]
     else:
       name = "nagaraj.srinivasa"
+  if not len(name):
+    sys.exit(0)
   uid = get_user_id(name)
+  if not uid:
+    print("user id for %s not found" % name)
+    sys.exit(0)
   print("User:\n\t%s, %s" % (name, uid))
   cs = get_clusters(uid)
+  if not cs:
+    print("No cluster found for %s" % name)
   for c in cs:
     print("%s (id = %s)\t%s" % (c['name'], c['id']['$oid'], c['hv_type']))
     if c['hv_type'] == 'vsphere':
